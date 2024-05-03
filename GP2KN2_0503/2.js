c420dedf2a4940b3c829a3ecd04eb204f22ae2ad
Zuti Roland <zuti.roland@gmail.com>
2024-05-03 17:46:13 +0200

    Update 2.js

--- GP2KN2_0503/2.js
+++ GP2KN2_0503/2.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $("#gomb1").click(function(){
     $("#lista11").hide();
-    $("#lista12").hide();
+    $("#lista21").hide();
     $("#link").hide();
   });
   
   $("#gomb2").click(function(){
     $("#lista11").hide();
-    $("#lista12").hide();
+    $("#lista21").hide();
     $("#link").hide();
     $("#gomb2").hide();
   });
@@ -15,20 +15,20 @@ $(document).ready(function(){
   $("#gomb3").click(function(){
     $("h1").hide();
     $("#lista11").hide();
-    $("#lista12").hide();
+    $("#lista21").hide();
     $("#link").hide();
   });
   
   $("#gomb4").click(function(){
     $("#lista11").hide();
-    $("#lista12").hide();
+    $("#lista21").hide();
     $("#link").hide();
     $("a").text("");
   });
   
   $("#gomb5").click(function(){
     $("#lista11").hide();
-    $("#lista12").hide();
+    $("#lista21").hide();
     $("table tr:even").hide();
     $("#link").hide();
   });
