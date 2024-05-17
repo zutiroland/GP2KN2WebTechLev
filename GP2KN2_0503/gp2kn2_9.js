a5855bdbf2142923ecc8f2ad69e0ff0afbab78e7
Zuti Roland <zuti.roland@gmail.com>
2024-05-17 11:25:09 +0200

    Update gp2kn2_9.js

--- GP2KN2_0503/gp2kn2_9.js
+++ GP2KN2_0503/gp2kn2_9.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
   $("#read-json").click(function () {
     $.getJSON("gp2kn2_orarend.json", function (data) {
       $('#stg').html('<h2>MISKOLCI EGYETEM</h2>');
-      data.forEach(function (course) {
-        $('#stg').append(`<p><b>Cím:</b></p>${course.cim.code} ${course.cim.city} ${course.cim.street}</p>`)
+      data.forEach(function (data) {
+        $('#stg').append(`<p><b>Cím:</b></p>${data.cim.code} ${data.cim.city} ${data.cim.street}</p>`)
         $('#stg').append('<p><b>Telefonszámok:</b></p>');
 
-        for (i = 0; i < course["phone number"].length; i++) {
-          $('#stg').append(`<li>${course["phone number"][i].type} ${course["phone number"][i].number}</li>`);
+        for (i = 0; i < data["phone number"].length; i++) {
+          $('#stg').append(`<li>${data["phone number"][i].type} ${data["phone number"][i].number}</li>`);
         }
 
         $('#stg').append('<p><h3>MI Órarend 2024 tavasz</h3><ul>');
-        $('#stg').append(`<p><b>Kurzusnév: </b>${course.kurzusnev}</p>`);
-        $('#stg').append(`<p><b>Oktató: </b>${course.oktato}</p>`);
-        $('#stg').append(`<p><b>Szak: </b>${course.szak}</p>`);
-        $('#stg').append(`<b>Időpont:</b><br>&nbsp;&nbsp;Nap:&nbsp;${course.idopont.nap}<br> &nbsp;&nbspTól: &nbsp;${course.idopont.tol}<br>&nbsp;&nbsp;Ig: &nbsp;${course.idopont.ig}<br>`);
-        $('#stg').append(`<p><b>Helyszín: </b>${course.helyszin}</p>`);
+        $('#stg').append(`<p><b>Kurzusnév: </b>${data.kurzusnev}</p>`);
+        $('#stg').append(`<p><b>Oktató: </b>${data.oktato}</p>`);
+        $('#stg').append(`<p><b>Szak: </b>${data.szak}</p>`);
+        $('#stg').append(`<b>Időpont:</b><br>&nbsp;&nbsp;Nap:&nbsp;${data.idopont.nap}<br> &nbsp;&nbspTól: &nbsp;${data.idopont.tol}<br>&nbsp;&nbsp;Ig: &nbsp;${data.idopont.ig}<br>`);
+        $('#stg').append(`<p><b>Helyszín: </b>${data.helyszin}</p>`);
         $('#stg').append('<br>')
         $('#stg').append('<hr>')
         $('#stg').append('<br>')
