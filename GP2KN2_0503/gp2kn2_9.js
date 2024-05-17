446e152c5068abaa451619ed678d61d413855df6
Zuti Roland <zuti.roland@gmail.com>
2024-05-17 11:19:57 +0200

    Update gp2kn2_9.js

--- GP2KN2_0503/gp2kn2_9.js
+++ GP2KN2_0503/gp2kn2_9.js
@@ -1,20 +1,25 @@
-$(document).ready(async function(){
-    const getJson = (path) => {
-        return new Promise((resolve) => {
-          $.getJSON("gp2kn2_orarend.json", function (data) {
-            setTimeout(() => {
-              resolve(data);
-            }, 1);
-          });
-        })
-      }
-      var result = await getJson('gp2kn2_orarend.json');
-      console.log(result);
+$(document).ready(function () {
+  $("#read-json").click(function () {
+    $.getJSON("gp2kn2_orarend.json", function (data) {
+      $('#stg').html('<h2>MISKOLCI EGYETEM</h2>');
+      data.forEach(function (course) {
+        $('#stg').append(`<p><b>Cím:</b></p>${course.cim.code} ${course.cim.city} ${course.cim.street}</p>`)
+        $('#stg').append('<p><b>Telefonszámok:</b></p>');
 
-      const keys = Object.keys(result);
-      const stg = document.querySelector(".stg");
-      var h3=document.createElement("h3");
-      stg.innerHTML = "";
-      $( ".stg" ).append( "<h2>MISKOLCI EGYETEM</h2>" );
-      $( ".stg" ).append( `<h3>${keys[0]}</h3>` );
- });
+        for (i = 0; i < course["phone number"].length; i++) {
+          $('#stg').append(`<li>${course["phone number"][i].type} ${course["phone number"][i].number}</li>`);
+        }
+
+        $('#stg').append('<p><h3>MI Órarend 2024 tavasz</h3><ul>');
+        $('#stg').append(`<p><b>Kurzusnév: </b>${course.kurzusnev}</p>`);
+        $('#stg').append(`<p><b>Oktató: </b>${course.oktato}</p>`);
+        $('#stg').append(`<p><b>Szak: </b>${course.szak}</p>`);
+        $('#stg').append(`<b>Időpont:</b><br>&nbsp;&nbsp;Nap:&nbsp;${course.idopont.nap}<br> &nbsp;&nbspTól: &nbsp;${course.idopont.tol}<br>&nbsp;&nbsp;Ig: &nbsp;${course.idopont.ig}<br>`);
+        $('#stg').append(`<p><b>Helyszín: </b>${course.helyszin}</p>`);
+        $('#stg').append('<br>')
+        $('#stg').append('<hr>')
+        $('#stg').append('<br>')
+      });
+    });
+  });
+});
